aa7d38673b457159fb256d6519733c1ac4e72470
OldRabbit736 <sylvan0212@gmail.com>
2022-11-20 14:06:41 +0900

    완료: 커넥션풀과 데이터소스 이해 | DataSource 예제2-커넥션 풀

--- follow-along/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ follow-along/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -29,13 +30,40 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); // 커넥션 풀에서 커넥션 생성 시간 대기
+
+        // 커넥션 풀에서 커넥션을 생성하는 작업은 애플리케이션 실행 속도에 영향을 주지 않기 위해 별도의 쓰레드에서 작동한다.
+        // 커넥션 풀이 다 생성도 전에 테스트가 종료되어 버리는 것을 방지하기 위해 Thread.sleep을 통해 대기 시간을 준다.
+        // 대기 시간 동안 커넥션 풀이 동작하는 쓰레드에서 커넥션이 생성되는 로그를 확인할 수 있다.
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
         // 이미 DataSource에는 URL, USERNAME, PASSWORD 정보를 가지고 있으며, 커넥션을 가지고 올 때마다 정보를 재사용한다.
         // --> 설정과 사용의 분리
         // DriverManager는 설정과 사용이 동시에 일어난다.
         // 설정과 사용의 분리를 통해 설정을 한 곳에만 유지시킬 수 있기 때문에 향후 변경에 더 유연하게 대처할 수 있다.
-        Connection con1 = dataSource.getConnection();
+        Connection con1 = dataSource.getConnection();   // Connection을 받을 때까지 대기할 수도 있다. 타임아웃도 설정 가능.
         Connection con2 = dataSource.getConnection();
+//        Connection con3 = dataSource.getConnection();
+//        Connection con4 = dataSource.getConnection();
+//        Connection con5 = dataSource.getConnection();
+//        Connection con6 = dataSource.getConnection();
+//        Connection con7 = dataSource.getConnection();
+//        Connection con8 = dataSource.getConnection();
+//        Connection con9 = dataSource.getConnection();
+//        Connection con10 = dataSource.getConnection();
+//        Connection con11 = dataSource.getConnection();
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
     }
